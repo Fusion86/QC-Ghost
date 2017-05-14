14b1fec416edb4c3474682e6c36a7e895a71f084
Wouter Visser <wouteravisser@hotmail.com>
2017-05-14 17:51:40 +0200

    Initial commit

--- QC-Ghost/Program.cs
+++ QC-Ghost/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace QC_Ghost
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "BethesdaNetLauncher.exe";
+            startInfo.Arguments = "bethesdanet://run/11"; // 11 = Quake Champions
+            Process.Start(startInfo);
+
+            DateTime startedAt = DateTime.Now;
+
+            // Wait for Quake Champions to launch or just continue after 20 seconds have passed
+            while (!IsQuakeChampionsRunning && (DateTime.Now - startedAt).TotalSeconds < 20)
+                Thread.Sleep(1000);
+
+            // Wait for Quake Champions to exit, or just skip this step if it never started
+            while (IsQuakeChampionsRunning)
+                Thread.Sleep(1000);
+
+            // And then close the Bethesda Launcher
+            CloseBethesdaLauncher();
         }
+
+        private static void CloseBethesdaLauncher()
+        {
+            // Close all instances of the bethesda launcher
+            foreach (Process process in Process.GetProcessesByName("BethesdaNetLauncher"))
+                process.Kill(); // with fire
+        }
+
+        private static bool IsQuakeChampionsRunning => Process.GetProcessesByName("QuakeChampions").Length > 0;
     }
 }
---
+++ README.md
@@ -0,0 +1,26 @@
+# QC-Ghost
+
+Don't bother with the Bethesda Launcher.
+
+![Example](https://i.imgur.com/UT6a9zi.png)
+
+
+## Features
+
+- [x] Steam Overlay
+- [x] Auto Closes the Bethesda Launcher
+
+
+## How to use
+
+1. Drop QC-Ghost.exe in the same folder as the Bethesda Launcher
+2. Add QC-Ghost.exe as a non-steam game in Steam
+3. (optional) Change the name in Steam and add the icon
+4. Launch the game and have fun
+
+
+## Good to know
+
+You might have to accept an UAC promt from Windows (you always have to do this, even if you launch it directly from the Bethesda Launcher)
+
+If you fail to start Quake Championship within 20 seconds then QC-Ghost will exit and you won't be 'In a non-steam game' anymore.
