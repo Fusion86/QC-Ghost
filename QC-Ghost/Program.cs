5386ccdb9c84129a1c17280c52ac20f995d6595c
Wouter Visser <wouteravisser@hotmail.com>
2017-05-18 22:29:02 +0200

    Kill launcher if it is already running and check whether the given launcher path is valid

--- QC-Ghost/Program.cs
+++ QC-Ghost/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
 
 namespace QC_Ghost
@@ -13,6 +14,15 @@ namespace QC_Ghost
 
             if (args.Length > 0)
                 bethesdaNetLauncherPath = args[0]; // Makes debuggin easier and allows users to place the exe wherever they want
+
+            if (!File.Exists(bethesdaNetLauncherPath)) {
+                Console.Error.WriteLine($"'{bethesdaNetLauncherPath}' is not a valid path!");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+
+            // We need to start the launcher ourself for the Steam Overlay to work, so lets start by killing it if it's already running
+            CloseBethesdaLauncher();
 
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = bethesdaNetLauncherPath;
